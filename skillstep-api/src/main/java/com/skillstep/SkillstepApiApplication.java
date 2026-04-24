0007281b42627531b67a08a854f3e07e3df9cc34
Sirinemn <sirinemn@outlook.fr>
2026-04-24 10:49:56 +0200

    first commit

---
+++ skillstep-api/src/main/java/com/skillstep/SkillstepApiApplication.java
@@ -0,0 +1,13 @@
+package com.skillstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillstepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillstepApiApplication.class, args);
+	}
+
+}
---
+++ skillstep-api/src/test/java/com/skillstep/SkillstepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillstepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
