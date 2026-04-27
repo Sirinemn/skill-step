e227718c7efca7397d6ef82c18e84d9711b37a14
Sirinemn <sirinemn@outlook.fr>
2026-04-27 16:04:19 +0200

    implement OAuthservice

--- skillstep-api/src/main/java/com/skillstep/auth/service/OAuthUserService.java
+++ skillstep-api/src/main/java/com/skillstep/auth/service/OAuthUserService.java
@@ -1,4 +1,27 @@
 package com.skillstep.auth.service;
 
+import com.skillstep.user.domain.User;
+import com.skillstep.user.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class OAuthUserService {
+
+    private final IUserService userService;
+
+    public User processOAuthUser(OAuth2User oAuth2User, String provider) {
+        return userService.findOrCreate(
+                oAuth2User.getAttribute("email"),
+                oAuth2User.getAttribute("given_name"),
+                oAuth2User.getAttribute("family_name"),
+                oAuth2User.getAttribute("picture"),
+                provider,
+                oAuth2User.getAttribute("sub") // "sub" = identifiant unique Google
+        );
+    }
 }
