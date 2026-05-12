a376af4a4f1d88da7c7dda2a107088ea308b9891
Sirinemn <sirinemn@outlook.fr>
2026-05-12 17:31:59 +0200

    Add placeholder components for dashboard, profile, journal, log-form

---
+++ skillstep-ui/src/app/features/learning-log/pages/log-form/log-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-form',
+  imports: [],
+  templateUrl: './log-form.component.html',
+  styleUrl: './log-form.component.scss'
+})
+export class LogFormComponent {
+
+}
