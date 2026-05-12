2c88afcf5f6b02c2c0dcf0891f8b927772c982b3
Sirinemn <sirinemn@outlook.fr>
2026-05-12 21:31:54 +0200

    update dashboard

--- skillstep-ui/src/app/features/dashboard/dashboard.routes.ts
+++ skillstep-ui/src/app/features/dashboard/dashboard.routes.ts
@@ -1,11 +1,13 @@
 // src/app/features/dashboard/dashboard.routes.ts
 import { Routes }    from '@angular/router';
 import { authGuard } from '../../core/guards/auth.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
     title: 'Dashboard',
     canActivate: [authGuard],
+    component: DashboardComponent
   },
 ];
