56dbd4fb39026cd3f8235249000acac4fa02b63b
Sirinemn <sirinemn@outlook.fr>
2026-05-12 22:11:01 +0200

    import routerModule

--- skillstep-ui/src/app/features/auth/pages/login/login.component.ts
+++ skillstep-ui/src/app/features/auth/pages/login/login.component.ts
@@ -3,10 +3,11 @@ import { AuthService } from '../../../../core/services/auth.service';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../../../../shared/components/navbar/navbar.component";
 import { FooterComponent } from "../../../../shared/components/footer/footer.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, FooterComponent, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
