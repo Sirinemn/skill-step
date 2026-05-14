2e0e7b1b009507158b4ef633b6244a7dca9d8e2c
Sirinemn <sirinemn@outlook.fr>
2026-05-14 10:23:36 +0200

    add title to dashboard routes

--- skillstep-ui/src/app/features/dashboard/dashboard.routes.ts
+++ skillstep-ui/src/app/features/dashboard/dashboard.routes.ts
@@ -1,11 +1,10 @@
-// src/app/features/dashboard/dashboard.routes.ts
 import { Routes }    from '@angular/router';
 import { authGuard } from '../../core/guards/auth.guard';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
+    title: 'Dashboard',
     canActivate: [authGuard],
     // Le layout enveloppe le dashboard
     loadComponent: () =>
