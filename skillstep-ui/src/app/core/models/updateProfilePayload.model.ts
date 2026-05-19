08eb4c261be33a7d9a3b543c80b2db9ed463e4af
Sirinemn <sirinemn@outlook.fr>
2026-05-19 22:56:22 +0200

    add updateProfilePayload model

---
+++ skillstep-ui/src/app/core/models/updateProfilePayload.model.ts
@@ -0,0 +1,7 @@
+export interface UpdateProfilePayload {
+  headline:    string | null;
+  bio:         string | null;
+  targetRole:  string | null;
+  linkedinUrl: string | null;
+  githubUrl:   string | null;
+}
