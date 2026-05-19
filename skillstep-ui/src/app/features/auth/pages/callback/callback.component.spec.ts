4e51f8eca731750e5fe1ead73443ccd5ddb75c72
Sirinemn <sirinemn@outlook.fr>
2026-05-19 10:11:43 +0200

    add callback test

--- skillstep-ui/src/app/features/auth/pages/callback/callback.component.spec.ts
+++ skillstep-ui/src/app/features/auth/pages/callback/callback.component.spec.ts
@@ -1,16 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { CallbackComponent } from './callback.component';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { AuthService } from '../../../../core/services/auth.service';
 describe('CallbackComponent', () => {
   let component: CallbackComponent;
   let fixture: ComponentFixture<CallbackComponent>;
+  const mockAuthService = {
+    handleCallback: jest.fn(),
+    logout: jest.fn(),
+  };
+  const mockActivatedRoute = {
+    snapshot: {
+      queryParamMap: {
+        get: jest.fn(),
+      },
+    },
+  };
+  const mockToken = 'token123';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CallbackComponent]
-    })
-    .compileComponents();
+      imports: [CallbackComponent, HttpClientTestingModule],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CallbackComponent);
     component = fixture.componentInstance;
@@ -19,5 +39,16 @@ describe('CallbackComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should handle callback with token', () => {
+    mockActivatedRoute.snapshot.queryParamMap.get.mockReturnValue(mockToken);
+    component.ngOnInit();
+    expect(mockAuthService.handleCallback).toHaveBeenCalledWith(mockToken);
+  });
+
+  it('should logout if no token', () => {
+    mockActivatedRoute.snapshot.queryParamMap.get.mockReturnValue(null);
+    component.ngOnInit();
+    expect(mockAuthService.logout).toHaveBeenCalled();
   });
 });
