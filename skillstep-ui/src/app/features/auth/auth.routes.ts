996262ee1661713e1db45458e3783080565a472b
Sirinemn <sirinemn@outlook.fr>
2026-05-19 12:32:06 +0200

    add public-layout route

--- skillstep-ui/src/app/features/auth/auth.routes.ts
+++ skillstep-ui/src/app/features/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CallbackComponent } from './pages/callback/callback.component';
 import { LoginComponent } from './pages/login/login.component';
 import { unauthGuard } from '../../core/guards/unauth.guard';
+import { PublicLayoutComponent } from '../../layouts/public-layout/public-layout.component';
 
 export const AUTH_ROUTES: Routes = [
   {
@@ -24,28 +25,14 @@ export const AUTH_ROUTES: Routes = [
     title: 'Callback',
     component: CallbackComponent
   },
-    {
-    path: 'fonctionnalites',
-    loadComponent: () =>
-      import('./pages/features-page/features-page.component')
-        .then(m => m.FeaturesPageComponent),
-  },
   {
-    path: 'a-propos',
-    loadComponent: () =>
-      import('./pages/about/about.component')
-        .then(m => m.AboutComponent),
-  },
-  {
-    path: 'confidentialite',
-     loadComponent: () =>
-      import('./pages/privacy/privacy.component')
-        .then(m => m.PrivacyComponent),
-  },
-  {
-    path: 'conditions',
-    loadComponent: () =>
-      import('./pages/terms/terms.component')
-        .then(m => m.TermsComponent),
-  },
+  path: '',
+  component: PublicLayoutComponent, // ← layout partagé
+  children: [
+    { path: 'fonctionnalites', loadComponent: () => import('./pages/features-page/features-page.component').then(m => m.FeaturesPageComponent) },
+    { path: 'a-propos',        loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+    { path: 'confidentialite', loadComponent: () => import('./pages/privacy/privacy.component').then(m => m.PrivacyComponent) },
+    { path: 'conditions',      loadComponent: () => import('./pages/terms/terms.component').then(m => m.TermsComponent) },
+  ]
+}
 ];
