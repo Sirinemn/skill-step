61d66d4af1261e781ae94a1e72dd2491ee99563d
Sirinemn <sirinemn@outlook.fr>
2026-05-22 11:06:23 +0200

    test : implement userservice tests

--- skillstep-ui/src/app/core/services/user.service.spec.ts
+++ skillstep-ui/src/app/core/services/user.service.spec.ts
@@ -1,16 +1,75 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { AuthService } from './auth.service';
+import { UpdateProfilePayload } from '../models/updateProfilePayload.model';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
+  const authServiceMock = {
+  setCurrentUser: jest.fn(),
+};
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        UserService,
+        { provide: AuthService, useValue: authServiceMock }
+      ]
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should fetch user profile', () => {
+    const mockUser = { id: 1, name: 'John Doe' };
+    service.getProfile().subscribe(user => {
+      expect(user).toEqual(mockUser);
+    });
+    const req = httpMock.expectOne(`${service['baseUrl']}/me`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser); // Simule la réponse du backend
+  });
+  it('should update user profile and call setCurrentUser', () => {
+    const payload = { bio: 'dev' } as UpdateProfilePayload;
+    const updatedUser = { id: 1, bio: 'dev' };
+    service.updateProfile(payload).subscribe(user => {
+      expect(user).toEqual(updatedUser);
+      expect(authServiceMock.setCurrentUser).toHaveBeenCalledWith(updatedUser);
+    });
+    const req = httpMock.expectOne(`${service['baseUrl']}/me`);
+    expect(req.request.method).toBe('PATCH');
+    expect(req.request.body).toEqual(payload);
+    req.flush(updatedUser);
+  });
+  it('should handle error', () => {
+
+    service.getProfile().subscribe({
+      next: () => fail('should fail'),
+      error: (err) => {
+        expect(err.status).toBe(500);
+      }
+    });
+
+    const req = httpMock.expectOne(
+      `${service['baseUrl']}/me`
+    );
+
+    req.flush(
+      'Server Error',
+      {
+        status: 500,
+        statusText: 'Server Error'
+      }
+    );
   });
 });
