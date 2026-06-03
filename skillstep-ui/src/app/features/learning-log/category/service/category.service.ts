5e378b8847632eaabb98c3d0a0a34e4d5b2e69a8
Sirinemn <sirinemn@outlook.fr>
2026-06-03 19:11:11 +0200

    implement category service

--- skillstep-ui/src/app/features/learning-log/category/service/category.service.ts
+++ skillstep-ui/src/app/features/learning-log/category/service/category.service.ts
@@ -1,15 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { CategoryResponse } from '../models/category-response.model';
+import { CategoryRequest } from '../models/category-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  private apiUrl = `${environment.apiUrl}/categories`;
+  private readonly baseUrl = `${environment.apiUrl}/categories`;
 
   constructor(private http: HttpClient) { }
 
- 
+  getCategories(): Observable<CategoryResponse[]> {
+    return this.http.get<CategoryResponse[]>(this.baseUrl);
+  }
+  createCategory(categoryRequest: CategoryRequest): Observable<CategoryResponse> {
+    return this.http.post<CategoryResponse>(this.baseUrl, categoryRequest);
+  }
+  updateCategory(id: number, categoryRequest: CategoryRequest): Observable<CategoryResponse> {
+    return this.http.put<CategoryResponse>(`${this.baseUrl}/${id}`, categoryRequest);
+  }
+  deleteCategory(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
