78d69a0637d0a5b1a6add633f91a2e44ec6686d1
Sirinemn <sirinemn@outlook.fr>
2026-06-16 12:15:32 +0200

    Implement dashboardService tests

--- skillstep-ui/src/app/features/dashboard/services/dashboard.service.spec.ts
+++ skillstep-ui/src/app/features/dashboard/services/dashboard.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { DashboardService } from './dashboard.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { DashboardStats } from '../models/dashboard-stats.model';
+import { DayActivity } from '../models/day-activity.model'
+import { CategoryStats } from '../models/category-stats.model';
 
 describe('DashboardService', () => {
   let service: DashboardService;
@@ -14,8 +17,44 @@ describe('DashboardService', () => {
     service = TestBed.inject(DashboardService);
     httpMock = TestBed.inject(HttpTestingController);
   });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should fetch dashboard stats', () => {
+    const dayActivity: DayActivity[] = [{
+      day: "23-05-2025",
+      date: "16-16-2026",
+      totalMinutes: 20,
+      logCount: 1
+    }];
+    const categoryStats : CategoryStats[] = [{
+      id: 1,
+      name: "Java",
+      color: "#FFFF",
+      totalMinutes: 30,
+      logCount:3,
+      percentage: 20
+    }]
+    const dashboardStatMock : DashboardStats = {
+      totalLogs:10,
+      totalMinutes:360,
+      activeCategories:5,
+      logsThisWeek:3,
+      streakDays:3,
+      activityLast7Days: dayActivity,
+      topCategories: categoryStats
+    }
+
+    service.loadStats().subscribe( dashboardStat => {
+      expect(dashboardStat).toEqual(dashboardStatMock);
+    });
+    
+    const req = httpMock.expectOne(`${service['apiUrl']}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dashboardStatMock);
+  })
 });
