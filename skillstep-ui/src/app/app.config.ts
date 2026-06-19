aa9c124796e7a210b271a5e6be06d4b18bfc751a
Sirinemn <sirinemn@outlook.fr>
2026-06-19 16:48:47 +0200

    add provide LOCAL_ID fr to correct Datepipe issue

--- skillstep-ui/src/app/app.config.ts
+++ skillstep-ui/src/app/app.config.ts
@@ -4,6 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr');
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([jwtInterceptor])
-    )
+    ),
+    { provide: LOCALE_ID, useValue: 'fr' }
   ]
 };
