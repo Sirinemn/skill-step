2373b9fa90794bb4458cee9e12d277f372d931e7
Sirinemn <sirinemn@outlook.fr>
2026-09-04 16:50:32 +0200

    fix(auth): wait for session initialization in unauth guard

--- skillstep-ui/src/app/core/guards/unauth.guard.ts
+++ skillstep-ui/src/app/core/guards/unauth.guard.ts
@@ -1,15 +1,27 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
+import { map } from "rxjs/internal/operators/map";
+import { take } from "rxjs/internal/operators/take";
+import { filter } from "rxjs/internal/operators/filter";
+import { toObservable } from "@angular/core/rxjs-interop";
 
 export const unauthGuard : CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated$()) {
-    router.navigate(['/dashboard']);
-    return false;
-  }
+  return toObservable(authService.isInitialized$).pipe(
 
-  return true;
+    filter(initialized => initialized),
+
+    take(1),
+
+    map(() => {
+
+      if (authService.isAuthenticated$()) {
+        return router.createUrlTree(['/dashboard']);
+      }
+      return true;
+    })
+  );
 };
