e688dd3529f48da7a3f1151244843fe1da64f2b9
Sirinemn <sirinemn@outlook.fr>
2026-09-11 21:16:43 +0200

    fix warnings inbuilds

--- skillstep-api/src/main/java/com/skillstep/user/mapper/UserMapper.java
+++ skillstep-api/src/main/java/com/skillstep/user/mapper/UserMapper.java
@@ -11,6 +11,15 @@ public interface UserMapper {
     UserProfileResponse toProfileResponse(User user);
 
     // MapStruct mappe tous les champs y compris les nulls
+    @Mapping(target = "id",         ignore = true)
+    @Mapping(target = "email",      ignore = true)
+    @Mapping(target = "firstName",  ignore = true)
+    @Mapping(target = "lastName",   ignore = true)
+    @Mapping(target = "provider",   ignore = true)
+    @Mapping(target = "providerId", ignore = true)
+    @Mapping(target = "avatarUrl",  ignore = true)
+    @Mapping(target = "createdAt",  ignore = true)
+    @Mapping(target = "updatedAt",  ignore = true)
     void updateUserFromRequest(UpdateProfileRequest request,
                                @MappingTarget User user);
 }
